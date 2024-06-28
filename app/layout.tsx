c727e4179b4b5e697624ebc1f24d70e94461a04c
Rajat Mahesh Gupta <guptarajat978@gmail.com>
2024-06-28 17:31:57 -0400

    Fixes #1 - Repo Documentation

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Rajat's Personal Portfolio",
+  description: "Personal Portfolio of Rajat Gupta built from Scratch",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            <h1>Hero Section</h1>
+        </div>
+    );
+};
+
+export default Page;
